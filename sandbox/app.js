187404fcbb7581a4fc54f96f1037e2b7f14c90dd
Danohmite <Danomite84@gmail.com>
2020-12-16 16:35:54 -0800

    primitive and referecence types

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,74 +1,35 @@
-// console.log ("hello world");
-// console.log(123);
-// const greeting = 'helllloooo world';
-// console.log(greeting);
-// console.log([1,2,3,4]);
-// console.log({a:1, b:2});
-// console.table({a:1, b:2});
+// Primitive Type
 
-// console.error('This is some error');
-// console.warn('warrrrning');
-// console.clear();
-// console.time('Hello')
-//     console.log ("hello world");
-//     console.log ("hello world");
-//     console.log ("hello world");
-//     console.log ("hello world");
-//     console.log ("hello world");
-// console.timeEnd('Hello');
+//String
+const name = 'John Doe';
 
-// var, let, const
+console.log(typeof name)
 
-// var name = 'Jogn Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
+// Number
+const age = 45;
+console.log(typeof age)
+//Boolean
+const hasKids = true;
+console.log(typeof hasKids);
 
-// Init var
-var greeting;
-console.log(greeting);
-greeting = 'Hello';
-console.log(greeting);
+// Null
+const car = null;
+// funny its been a bug of null since the beginning of js
 
-// vars can only have letters, numbers, _, $
-// Can not start with number
-// var $name = 'John';
-// wont work unless using it for jQuery
-var _name = 'john'
-// not best practice
+//Undefined
+let test;
 
-// Multi word vars
+// Symbol
+const sym = Symbol();
 
-var firstName = 'John'; // Camel case ****** best practice
-var first_name = 'Sara' // underscore method
-var FirstName = 'Tom' // Pascal case *** good for OSS
-var firstname; // not awesome
-
-// let
-// works similar to var
-let name = 'Jogn Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
-
-// CONST
-// const name = 'John';
-// console.log(name);
-// Can not reassign 
-// name = 'Sara'
-// Have to assign a value
-// const greeting; does not work!
-
-const person = {
-    name: 'John',
-    age: 30
+// REFERENCE TYPES - OBjects
+const hobbies = ['movies', 'music'];
+//Object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
 }
+const today = new Date();
 
-person.name = 'Sara';
-person.age = 32;
-
-// console.log(person);
-
-const numbers = [1,2,3,4,5];
-numbers.push(6);
-console.log(numbers)
+console.log(today);
+console.log(typeof today);
---
+++ sandbox/section2/section2.js
@@ -0,0 +1,74 @@
+// console.log ("hello world");
+// console.log(123);
+// const greeting = 'helllloooo world';
+// console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+
+// console.error('This is some error');
+// console.warn('warrrrning');
+// console.clear();
+// console.time('Hello')
+//     console.log ("hello world");
+//     console.log ("hello world");
+//     console.log ("hello world");
+//     console.log ("hello world");
+//     console.log ("hello world");
+// console.timeEnd('Hello');
+
+// var, let, const
+
+// var name = 'Jogn Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// Init var
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+// vars can only have letters, numbers, _, $
+// Can not start with number
+// var $name = 'John';
+// wont work unless using it for jQuery
+var _name = 'john'
+// not best practice
+
+// Multi word vars
+
+var firstName = 'John'; // Camel case ****** best practice
+var first_name = 'Sara' // underscore method
+var FirstName = 'Tom' // Pascal case *** good for OSS
+var firstname; // not awesome
+
+// let
+// works similar to var
+let name = 'Jogn Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// CONST
+// const name = 'John';
+// console.log(name);
+// Can not reassign 
+// name = 'Sara'
+// Have to assign a value
+// const greeting; does not work!
+
+const person = {
+    name: 'John',
+    age: 30
+}
+
+person.name = 'Sara';
+person.age = 32;
+
+// console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers)
