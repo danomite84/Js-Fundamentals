5d28e33516b66c268f15875e189b938ff422f142
Danohmite <Danomite84@gmail.com>
2020-12-16 16:15:36 -0800

    practicing console.log

---
+++ sandbox/app.js
@@ -0,0 +1,18 @@
+console.log ("hello world");
+console.log(123);
+const greeting = 'helllloooo world';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.warn('warrrrning');
+console.clear();
+console.time('Hello')
+    console.log ("hello world");
+    console.log ("hello world");
+    console.log ("hello world");
+    console.log ("hello world");
+    console.log ("hello world");
+console.timeEnd('Hello');
