090c15b163ce5f339206fa4282a704ee4215f88d
Danohmite <Danomite84@gmail.com>
2020-12-16 17:02:16 -0800

    number & Math Object

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,46 +1,32 @@
-//Conversions
+// Math var
+
+const num1 = 100;
+const num2 = 50;
 let val;
 
-// Number to string;
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
 
-// val = 5;
-val = String(555);
-val = String (4+4);
-//Boolean
-val = String(true);
-//Date to string
-val = String(new Date());
-//Array to string
-val = String([1,2,3,4]);
-// toString();
-val= (5).toString();
-val = (true).toString();
-// String to number
-val = Number('5');
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number('hello');
-val = Number([1,2,3]);
-
-val = parseInt('100.30');
-// parse integer
-val = parseFloat('100.30');
-// Float default gives 1 decimal.
-// using toFixed will add as many extensions after the decimal
-
-// Output
-// console.log(val);
-// console.log(typeof val);
-// console.log(val.length);
-// console.log(val.toFixed(2));
-// New one for me .toFixed will add a decimal point but only works
-// on numbers. if u add it to a string will create an error
-
-//Type cohersion
-const val1 = String(5);
-const val2 = 6;
-const sum = val1 + val2;
-
-console.log(sum);
-console.log(typeof sum);
+// Math Object
+val = Math.PI;
+//Pi'
+val = Math.E;
+// Eulers Method
+val = Math.round(2.4);
+val = Math.floor(2.4);
+val = Math.sqrt(64);
+val = Math.abs(-3)
+//absolute, never used this really.
+// so if i put a negative number in it will give the positive
+// or absolute number in
+val = Math.pow(8, 2);
+// power of ie 8 to the 2nd or 8^2
+val = Math.min(2,33,4,1,55,6,3,-2);
+val = Math.max(2,33,4,1,55,6,3,-2);
+val = Math.random();
+val = Math.floor(Math.random() * 20 + 1);
+console.log(val)
--- sandbox/section2/section2.js
+++ sandbox/section2/section2.js
@@ -109,3 +109,50 @@ const today = new Date();
 
 console.log(today);
 console.log(typeof today);
+
+//Conversions
+let val;
+
+// Number to string;
+
+// val = 5;
+val = String(555);
+val = String (4+4);
+//Boolean
+val = String(true);
+//Date to string
+val = String(new Date());
+//Array to string
+val = String([1,2,3,4]);
+// toString();
+val= (5).toString();
+val = (true).toString();
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1,2,3]);
+
+val = parseInt('100.30');
+// parse integer
+val = parseFloat('100.30');
+// Float default gives 1 decimal.
+// using toFixed will add as many extensions after the decimal
+
+// Output
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// console.log(val.toFixed(2));
+// New one for me .toFixed will add a decimal point but only works
+// on numbers. if u add it to a string will create an error
+
+//Type cohersion
+const val1 = String(5);
+const val2 = 6;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
